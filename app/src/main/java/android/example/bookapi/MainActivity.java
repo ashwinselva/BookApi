07bfc1dfc8599c0e14e1cc310d4830df0591bac3
ashwin <ashwin.selvendran@cognizant.com>
2022-02-22 15:23:01 -0500

    shared preferences

--- app/src/main/java/android/example/bookapi/MainActivity.java
+++ app/src/main/java/android/example/bookapi/MainActivity.java
@@ -2,6 +2,7 @@ package android.example.bookapi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -22,5 +23,33 @@ public class MainActivity extends AppCompatActivity {
         mTitleText=findViewById(R.id.bookName);
         mAuthorText=findViewById(R.id.authorName);
         new FetchBookTask(mTitleText, mAuthorText).execute(queryString);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveData();
+    }
+
+    private void saveData() {
+
+        String data = mBookInput.getText().toString();
+        //create a file using shared preferences
+        SharedPreferences preferences = getSharedPreferences("cognizant_prefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("bookname",data);
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        restoreData();
+    }
+
+    private void restoreData() {
+        SharedPreferences preferences = getSharedPreferences("cognizant_prefs",MODE_PRIVATE);
+        String dataRead = preferences.getString("bookname","");    //getString(s => input parameter, s1=> default input if s is not available)
+        mBookInput.setText(dataRead);
     }
 }
